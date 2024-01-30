99d3726822d0be4d71ca3936e3be73c3d6a1392c
Bohdan Yevsiienko <vad99992@gmail.com>
2024-01-30 17:43:21 +0200

    finish 6hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1 className="title">Phone book</h1>
+      <ContactForm />
+      <h2 className="title">Contacts</h2>
+      <Filter />
+      <ContactList />
+    </>
   );
 };
